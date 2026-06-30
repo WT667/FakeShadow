cf1be01eed8ae7c4a710c3601bd0a365be08cefc
WT667 <54269231@qq.com>
2026-06-30 03:14:22 +0000

    Optimize project structure, add core hook examples, and update README style

---
+++ app/src/main/java/com/fakeshadow/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fakeshadow;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // TODO: Implement UI for location selection and simulation control
+    }
+}
